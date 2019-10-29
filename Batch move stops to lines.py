8f5dbc50d960aa8e6c7a7f95b4223aaa033eb477
ixinyue <42425064+ixinyue94@users.noreply.github.com>
2019-10-29 14:48:47 +0800

    Update and rename Match stops with lines.py to Batch move stops to lines.py

--- Batch move stops to lines.py
+++ Batch move stops to lines.py
@@ -1,6 +1,7 @@
 # 检查线是否有重复
 # 按字段批量导出各线至单独的shp文件
 # 按字段批量导出各线的点至单独的shp文件
+# 该方法将改掉原文件，请注意备份
 
 import arcpy
 import os.path
@@ -25,12 +26,11 @@ for n in range(0,190): #若有190条线
     line = path_line[n]
     arcpy.Near_analysis(infc, line, "", "true") #生成线上距离最近的点（原点shp文件属性表增加字段"NEAR_X", "NEAR_Y"）
     rows = arcpy.UpdateCursor(infc)
-    for row in rows:
+    for row in rows: #更新原点坐标x,y为"NEAR_X", "NEAR_Y"，实现将点移至线上
         if row.NEAR_DIST > 0:
             geo = row.shape.getPart()
             geo.X = row.NEAR_X
             geo.Y = row.NEAR_Y
             row.shape = arcpy.PointGeometry(geo)
             rows.updateRow(row)
-    arcpy.DeleteField_management(infc, ['NEAR_FID', 'NEAR_DIST', 'NEAR_X', 'NEAR_Y', 'NEAR_ANGLE'])
-    
+    arcpy.DeleteField_management(infc, ['NEAR_FID', 'NEAR_DIST', 'NEAR_X', 'NEAR_Y', 'NEAR_ANGLE']) #删除Near分析时生成的多余字段
